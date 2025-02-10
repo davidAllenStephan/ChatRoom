f443deb3797df76c1b7bfe95e5ed011a1eee56e2
davidAllenStephan <david.marino03@hotmail.com>
2025-02-10 13:50:09 -0600

    added basic multithreading

--- client/client.py
+++ client/client.py
@@ -5,8 +5,9 @@ PORT = 65432
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
-    s.close()
-
-print('Received', repr(data))
+    while True:
+        val = input("Enter your value: ")
+        val_b = val.encode('ascii')
+        s.sendall(val_b)
+        data = s.recv(1024)
+        print('Received', repr(data))
--- server/server.py
+++ server/server.py
@@ -1,17 +1,23 @@
 import socket
+import threading
 
 HOST = '127.0.0.1'
 PORT = 65432
 
+
+def recieve(conn):
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        conn.sendall(data)
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-    conn, addr = s.accept()
-    with conn:
+    while True:
+        conn, addr = s.accept()
         print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
-    s.close()
+        t1 = threading.Thread(target=recieve, args=(conn,))
+        t1.start()
